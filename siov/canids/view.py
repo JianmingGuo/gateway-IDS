3e759113144699030fb80b69d42749accdc20f3c
JianmingGuo <jsguojianming@sjtu.edu.cn>
2020-11-26 17:01:37 +0800

    ming

--- siov/canids/view.py
+++ siov/canids/view.py
@@ -10,6 +10,7 @@ from django.http import JsonResponse
 import json
 
 from django.views.decorators.csrf import csrf_exempt
+from openpyxl import load_workbook
 
 def canids_uplaod_normal(req):
     shutil.rmtree('./canids/train')
@@ -40,7 +41,7 @@ def canids_uplaod_test(req):
         if not file:
             return render(req,"canids.html",{"errinf1":"No files for upload!"})
 
-        f = open('./canids/upload/testfile.csv','wb+')
+        f = open('./canids/upload/testfile.xlsx','wb+')
         for line in file.chunks():
             f.write(line)
         f.close()
@@ -56,25 +57,41 @@ def canids_uplaodsuccess1(req):
 def canids_uplaodsuccess2(req):
     return render(req,'canids.html',{"errinf1":"successful file!"})
 
+def get_xlsx(filename):
+    workbook = load_workbook(filename)
+    booksheet = workbook.active
+    rows = booksheet.rows
+
+    data = []
+
+    for row in rows:
+        line = [col.value for col in row]
+        data.append(line)
+    return data
+
 
 def GetList(request):
+
+    filename = './canids/upload/testfile.xlsx'
+    data = get_xlsx(filename)
+
 
     list = []
 
     if request.is_ajax():
         list.append({'id':str('1'),'model':'basic classifier' })
-        list.append({"pid":str('1'),'id':'1_1','model':'LSTM','possibility':'99.15%'  })
-        list.append({"pid":str('1'),'id':'1_2','model':'ANN','possibility':'98.32%'  })
-        list.append({"pid":str('1'),'id':'1_3','model':'HTM','possibility':'98.14%'  })
-        list.append({"pid":str('1'),'id':'1_2','model':'CART','possibility':'98.23%'  })
-        list.append({"pid":str('1'),'id':'1_2','model':'转移矩阵','possibility':'98.07%'  })
+        list.append({"pid":str('1'),'id':'1_1','model':'LSTM','possibility':str(data[0][1])  })
+        list.append({"pid":str('1'),'id':'1_2','model':'ANN','possibility':str(data[1][1])  })
+        list.append({"pid":str('1'),'id':'1_3','model':'HTM','possibility':str(data[2][1])  })
+        list.append({"pid":str('1'),'id':'1_2','model':'CART','possibility':str(data[3][1])  })
+        list.append({"pid":str('1'),'id':'1_2','model':'转移矩阵','possibility':str(data[4][1])  })
 
-        list.append({'id':str('2'),'model':'emseble classifier(possibility:98.39%,DOS)' })
+        list.append({'id':str('2'),'model':'emseble classifier(possibility:'+data[5][0]+','+str(data[5][1])+')' })
         list.append({"pid":str('2'),'id':'2_1','model':'Attack Type','possibility':'Emsemble results'  })
-        list.append({"pid":str('2'),'id':'2_2','model':'DoS','possibility':'0.984224'  })
-        list.append({"pid":str('2'),'id':'2_3','model':'Fuzzy','possibility':'0.984103'  })
-        list.append({"pid":str('2'),'id':'2_4','model':'Spoofing','possibility':'0.984200'  })
-        list.append({"pid":str('2'),'id':'2_5','model':'Replay','possibility':'0.983979'  })
+        list.append({"pid":str('2'),'id':'2_2','model':'DoS','possibility':str(data[6][1])  })
+        list.append({"pid":str('2'),'id':'2_3','model':'Fuzzy','possibility':str(data[7][1])  })
+        list.append({"pid":str('2'),'id':'2_4','model':'Spoofing','possibility':str(data[8][1])  })
+        list.append({"pid":str('2'),'id':'2_5','model':'Replay','possibility':str(data[9][1])  })
             
 
     re = {'list':json.dumps(list)}
